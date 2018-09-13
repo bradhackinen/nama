261758b63211c326b86696f575b64c2de1973873
Brad Hackinen <bradhackinen@users.noreply.github.com>
2018-09-13 09:06:06 -0700

    improved handling of acronyms and apostrophes

--- hashes.py
+++ hashes.py
@@ -6,7 +6,8 @@ def basicHash(s):
     '''
     s = s.lower()
     s = re.sub(' & ',' and ',s)
-    s = re.sub(r'[\s\.,:;/\'"\(\)]+',' ',s)
+    s = re.sub(r'(?<=\S)[\'’´\.](?=\S)','',s)
+    s = re.sub(r'[\s\.,:;/\'"`´‘’“”\(\)_—\-]+',' ',s)
     s = s.strip()
 
     return s
@@ -23,6 +24,7 @@ def corpHash(s):
     s = basicHash(s)
     if s.startswith('the '):
         s = s[4:]
+
     s = re.sub('( (group|holding(s)?( co)?|inc(orporated)?|ltd|l ?l? ?[cp]|co(rp(oration)?|mpany)?|s[ae]|plc))+$','',s,count=1)
 
     return s
