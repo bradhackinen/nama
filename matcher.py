837b09e8efaa8e6e015df3afbd83796e3e16d464
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-08 19:54:30 -0400

    Add methods to compare graphs using edge, component, and bp measures

--- matcher.py
+++ matcher.py
@@ -166,37 +166,117 @@ class Matcher():
                 continue
 
             elif len(component) == 2:
-                s0,s1 = component
-                impacts[(s0,s1)] = self.counts[s0]*self.counts[s1]
+                s0, s1 = component
+                impacts[(s0, s1)] = self.counts[s0]*self.counts[s1]
 
             else:
                 G_c = G.subgraph(component)
 
-                for s0,s1 in nx.algorithms.bridges(G_c):
+                for s0, s1 in nx.algorithms.bridges(G_c):
                     G_b = G_c.copy()
-                    G_b.remove_edge(s0,s1)
+                    G_b.remove_edge(s0, s1)
 
                     bridgedComponents = list(nx.connected_components(G_b))
                     assert len(bridgedComponents) == 2
 
-                    counts = [sum(self.counts[s] for s in c) for c in bridgedComponents]
+                    counts = [sum(self.counts[s] for s in c)
+                              for c in bridgedComponents]
                     impact = counts[0]*counts[1]
 
-                    impacts[(s0,s1)] = impact
+                    impacts[(s0, s1)] = impact
 
         return impacts
 
-    def matchImpactsDF(self,string=None):
+    def matchImpactsDF(self, string=None):
         impacts = self.matchImpacts(string=string)
-        df = pd.DataFrame([(s0,s1,impact) for (s0,s1),impact in impacts.items()],columns=['string0','string1','impact'])
+        df = pd.DataFrame([(s0, s1, impact) for (
+            s0, s1), impact in impacts.items()], columns=['string0', 'string1', 'impact'])
 
-        df = pd.merge(df,self.matchesDF(string=string))
+        df = pd.merge(df, self.matchesDF(string=string))
 
-        df = df.sort_values('impact',ascending=False)
+        df = df.sort_values('impact', ascending=False)
 
         return df
 
-    def merge(self,leftDF,rightDF,how='inner',on=None,left_on=None,right_on=None,score_pairs=True,component_column_name='component',suffixes=('_x','_y')):
+    def edge_compare(self, matcher):
+
+        n = len(self.strings(min_count=0))
+
+        edges_A = {tuple(sorted((i, j))) for i, j in self.G.edges()}
+        edges_B = {tuple(sorted((i, j))) for i, j in matcher.G.edges()}
+
+        n_misses = len(edges_A ^ edges_B)
+
+        return 1 - 2*n_misses/(n**2 - n)
+
+    def component_compare(self, matcher):
+
+        n = len(self.strings(min_count=0))
+
+        components_A = [set(c) for c in self.components()]
+        components_B = [set(c) for c in matcher.components()]
+
+        n_misses = 0
+        for c_A, c_B in list(product(components_A, components_B)):
+            n_misses += len(c_A & c_B) * len(c_A ^ c_B)
+
+        return round(abs(1 - 2*n_misses/(n**2 - n)), 3)
+
+    def bp_compare(self, toCompare):
+        g1 = self.G
+        g2 = toCompare.G
+
+        g1A = nx.to_numpy_matrix(g1)
+        g2A = nx.to_numpy_matrix(g2)
+
+        g1L = nx.laplacian_matrix(g1).todense()
+        g2L = nx.laplacian_matrix(g2).todense()
+
+        g1D = np.add(g1L, g1A)
+        g2D = np.add(g2L, g2A)
+
+        g1I = np.identity(len(g1))
+        g2I = np.identity(len(g2))
+
+        g1_HF = self._compute_homophily_factor(g1D)
+        g2_HF = self._compute_homophily_factor(g2D)
+
+        (g1a, g1c_prime) = self._compute_FaBP_constants_from_homophily_factor(g1_HF)
+        (g2a, g2c_prime) = self._compute_FaBP_constants_from_homophily_factor(g2_HF)
+
+        g1_epsilon = 1 / (1 + np.where(g1D == np.amax(g1D))[0][0])
+        g2_epsilon = 1 / (1 + np.where(g2D == np.amax(g2D))[0][0])
+
+        # g1_to_invert = np.add(g1I, g1a * g1D, -g1c_prime * g1A)
+        # g2_to_invert = np.add(g2I, g2a * g2D, -g2c_prime * g2A)
+        g1_to_invert = np.add(g1I, g1_epsilon**2 * g1D, -g1_epsilon * g1A)
+        g2_to_invert = np.add(g2I, g2_epsilon**2 * g2D, -g2_epsilon * g2A)
+
+        g1_inverse = np.linalg.inv(g1_to_invert)
+        g2_inverse = np.linalg.inv(g2_to_invert)
+
+        dist = np.linalg.norm(g1_inverse - g2_inverse)
+
+        s = 1 / (1 + dist)
+
+        return s
+
+    def _compute_homophily_factor(self, D):
+        one_norm = 1 / (2 + 2 * np.where(D == np.amax(D))[0][0])
+        c1 = 2 + np.sum(D)
+        c2 = np.sum(np.square(D)) - 1
+        frobenius_norm = math.sqrt(
+            (-c1 + math.sqrt(c1**2 + 4 * c2)) / (8 * c2))
+        return max(one_norm, frobenius_norm)
+
+    def _compute_FaBP_constants_from_homophily_factor(self, HF):
+        about_half_HF = HF - 0.5
+        four_HF_squared = 4 * about_half_HF**2
+        a = (four_HF_squared) / (1 - four_HF_squared)
+        c_prime = (2 * about_half_HF) / (1 - four_HF_squared)
+        return (a, c_prime)
+
+    def merge(self, leftDF, rightDF, how='inner', on=None, left_on=None, right_on=None, score_pairs=True, component_column_name='component', suffixes=('_x', '_y')):
 
         if ((left_on is None) or (right_on is None)) and (on is None):
             raise Exception('Must provide column(s) to merge on')
